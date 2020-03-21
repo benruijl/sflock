3f43ba227852250a7b27612f4fa27f5d4a8504aa
fishfishfish2104 <fishfishfish2104@gmail.com>
2020-03-21 19:35:16 +1300

    fix crypt causing segfault

--- sflock.c
+++ sflock.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <crypt.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/ioctl.h>
@@ -110,7 +111,7 @@ main(int argc, char **argv) {
             }
             else
                 if (!strcmp(argv[i], "-v")) 
-                    die("sflock-"VERSION", © 2015 Ben Ruijl\n");
+                    die("sflock-\"VERSION\", © 2015 Ben Ruijl\n");
                 else 
                     if (!strcmp(argv[i], "-h")) 
                         showline = 0;
